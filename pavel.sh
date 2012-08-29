45345090f76aced317feb0c496a5544fa8f84e85
Павел Елькин <pavel@ideco.ru>
2012-08-29 14:13:13 +0600

    Grouped commands in git config script according to their purpose.

--- pavel.sh
+++ pavel.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# View 
+git config --global color.ui "auto"
+git config --global log.decorate "full"
+git config --global grep.lineNumber "true" # enable -n option by default
+
+# Editors
 git config --global core.editor "vim"
 git config --global merge.tool "meld"
+
+# Encoding
 git config --global i18n.commitencoding "utf8"
 git config --global i18n.logoutputencoding "utf8"
-git config --global color.ui "auto"
+
+# Aliases
 git config --global alias.graph "log --graph" 
 git config --global alias.oneline "log --graph --oneline"
 git config --global alias.list "show --pretty=format: --name-only"
-git config --global log.decorate "full"
